683ceb6f0eef39f0728cbba712d286e803ae073f
MD Sumon <thesumonart.bd@gmail.com>
2023-12-30 15:04:50 +0600

    create basic stracture

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Header from "./components/Header";
+
+function App() {
+  return (
+    <>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="about" element={<About />} />
+      </Routes>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Header.jsx
@@ -0,0 +1,7 @@
+import styled from "styled-components";
+
+const Header = () => {
+  return <HeaderBar>Header</HeaderBar>;
+};
+const HeaderBar = styled.header``;
+export default Header;
