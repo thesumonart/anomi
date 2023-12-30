ba83c669e7a41ce742ff1fca49b1269a056bbdbc
MD Sumon <thesumonart.bd@gmail.com>
2023-12-30 16:40:56 +0600

    add nav section

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,100 @@
 import styled from "styled-components";
-
+import { NavLink, Link } from "react-router-dom";
+import { CiBoxList } from "react-icons/ci";
 const Header = () => {
-  return <HeaderBar>Header</HeaderBar>;
+  return (
+    <HeaderBar>
+      <div className="container">
+        <nav className="NavBar">
+          <Link to={"/"}>
+            <LogoImg src="/temp_logo.svg"></LogoImg>
+          </Link>
+          <MenuList>
+            <MenuItems>
+              <NavLink to={"/"}>Home</NavLink>
+            </MenuItems>
+            <MenuItems>
+              <NavLink to={"/about"}>About</NavLink>
+            </MenuItems>
+            <MenuItems>
+              <NavLink to={"/protfolio"}>portfolio</NavLink>
+            </MenuItems>
+            <MenuItems>
+              <NavLink to={"/blog"}>blog</NavLink>
+            </MenuItems>
+            <MenuItems>
+              <NavLink to={"/contact"}>contact</NavLink>
+            </MenuItems>
+          </MenuList>
+          <button>
+            <CiBoxList />
+          </button>
+        </nav>
+      </div>
+    </HeaderBar>
+  );
 };
-const HeaderBar = styled.header``;
+const HeaderBar = styled.header`
+  background: #f7f7f7;
+  box-shadow: rgba(0, 0, 0, 0.1) 0px 20px 25px -5px,
+    rgba(0, 0, 0, 0.04) 0px 10px 10px -5px;
+  height: 7.5rem;
+  position: sticky;
+  top: 0;
+  .container nav {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+
+    button {
+      cursor: pointer;
+      border: 0;
+      outline: 0;
+      height: 2.2rem;
+      width: 2.2rem;
+      svg {
+        width: 100%;
+        height: 100%;
+      }
+    }
+  }
+`;
+const LogoImg = styled.img`
+  /* width: 250px; */
+  height: 7.5rem;
+`;
+const MenuList = styled.ul`
+  list-style: none;
+  display: flex;
+  align-items: center;
+`;
+const MenuItems = styled.li`
+  margin: 0 20px;
+
+  a {
+    color: #000;
+    text-decoration: none;
+    font-family: "Poppins", sans-serif;
+    font-size: 1.8rem;
+    text-transform: uppercase;
+    position: relative;
+
+    &::before {
+      content: "";
+      position: absolute;
+      bottom: 50%;
+      transform: translateY(50%);
+      left: -1.2rem;
+      background-color: #4169e1;
+      height: 0rem;
+      border-radius: 50%;
+      width: 0rem;
+      transition: all 0.25s ease-out;
+    }
+    &:hover::before {
+      width: 0.7rem;
+      height: 0.7rem;
+    }
+  }
+`;
 export default Header;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,62 @@
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div>
+      Home Lorem, ipsum dolor sit amet consectetur adipisicing elit. Itaque
+      beatae rerum esse asperiores rem laborum fugiat voluptate iusto facere
+      incidunt, iure nihil eveniet ipsam doloremque dolor possimus eum enim?
+      Praesentium at, sequi maxime libero dolore vero placeat incidunt quo
+      quasi, doloremque voluptatum temporibus. Explicabo, porro. Tempora
+      veritatis consequuntur mollitia quidem, quos tenetur sint assumenda iste
+      molestiae iusto facilis, voluptatum ab quam incidunt quasi labore
+      distinctio iure magni. Laborum necessitatibus iste voluptatum repellat
+      autem officiis consequatur rem repudiandae quidem nulla ratione quo
+      officia, neque pariatur fugit. Ratione possimus aliquid neque? Cum
+      mollitia maxime porro provident nemo, iure, nisi iste modi, facere a
+      accusantium. Atque quaerat nostrum earum. Quas perferendis error fugit
+      expedita vitae molestiae quia numquam. Sed consectetur nulla ratione
+      cupiditate quas labore in, aut, eum minus voluptatum sit fugit numquam,
+      fuga veniam ea blanditiis ducimus amet aliquam quae quis nemo quibusdam
+      aspernatur? Dicta sint, odit rem fuga minima at blanditiis ex officia
+      explicabo, optio quisquam rerum. Tenetur numquam distinctio, aspernatur
+      unde magnam facilis porro omnis iure! Praesentium vero sequi eveniet
+      perferendis nemo dolor molestiae aspernatur explicabo hic quibusdam quae
+      corrupti accusantium, commodi impedit tempora voluptatibus dolorem.
+      Recusandae, maxime quae! Officiis officia enim, ipsam quis ullam placeat
+      illum commodi error sapiente facilis repellendus aliquam voluptate facere,
+      iure sed praesentium asperiores assumenda, ad cupiditate? Sunt vero
+      aspernatur necessitatibus quia consequuntur et totam, debitis quod, enim
+      omnis blanditiis quis eligendi eveniet, inventore unde animi ex commodi
+      fuga? Quia, dolore quis ea neque molestias id aperiam quasi expedita
+      ullam, ab exercitationem molestiae odio perspiciatis? Quo blanditiis
+      aliquid autem, deleniti asperiores nihil, dignissimos ab nemo totam
+      repudiandae magni harum maxime consectetur, eligendi ea aperiam est iste
+      debitis eveniet ullam aut alias similique praesentium? Rem magnam iusto
+      obcaecati repellendus. Asperiores pariatur neque quo eum repudiandae
+      alias, minima ad reprehenderit repellendus placeat nihil dolor veniam cum
+      similique molestiae? Asperiores sequi modi consequatur ex neque inventore
+      soluta voluptate sunt ipsam, omnis harum voluptatem provident deserunt
+      incidunt iusto impedit numquam autem ad, fuga quaerat. Eius enim facilis
+      quaerat perspiciatis molestiae adipisci veniam ex officiis ut fugit?
+      Accusamus perferendis quisquam vel dolorem sint recusandae a, enim alias
+      eveniet adipisci itaque iusto eius harum id delectus, tempora error quos
+      modi iure porro, repellendus suscipit consectetur deserunt nostrum.
+      Commodi iste dolor error sed, enim esse in dicta! Harum fugiat minus modi,
+      voluptate laudantium quaerat placeat porro mollitia tenetur a vero autem
+      atque provident unde quae aut excepturi? Explicabo quae earum sit alias
+      voluptatum. Pariatur delectus deserunt cumque, praesentium sint ea maxime
+      natus corrupti similique totam. Nesciunt dignissimos doloribus eos
+      exercitationem animi labore temporibus consequatur repellat error,
+      consequuntur sapiente sit dolorem quaerat itaque quisquam amet. Nam
+      quaerat accusamus impedit? Maxime quaerat vitae voluptatibus ratione
+      animi, nisi dolores excepturi, explicabo laudantium dolorem hic architecto
+      praesentium tempora, labore numquam. A dolores molestias sed dolorem autem
+      at tenetur, consequuntur adipisci itaque expedita facilis molestiae
+      laborum deleniti voluptates modi maxime delectus ex aspernatur animi
+      numquam officiis earum. Explicabo quidem quae saepe ducimus magnam hic
+      facere. Sapiente illo aliquam, quis omnis accusamus ullam voluptates
+      necessitatibus ratione earum saepe.
+    </div>
+  );
 };
 
 export default Home;
